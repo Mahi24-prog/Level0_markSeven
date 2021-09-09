d320ad485d2f792eb1aead1384be0586b8444754
Mahendra Balu Pawar <mahendrapawar444666@gmail.com>
2021-09-09 20:09:28 +0530

    basic logic and design

---
+++ app.js
@@ -0,0 +1,18 @@
+var inputText = document.querySelector("#input-txt");
+var transBtn = document.querySelector(".trans-btn");
+var outputText = document.querySelector(".output-box");
+
+var url = "https://api.funtranslations.com/translate/pirate.json";
+
+
+transBtn.addEventListener("click", translate)
+
+function convertUrl(txt){
+    return url + "?" + "text=" + txt;
+}
+
+function translate(){
+    var finalUrl = convertUrl(inputText.value)
+
+    fetch(finalUrl).then(response => response.json()).then(json => {outputText.innerText = json.contents.translated}).catch((err)=>{alert(err)})
+}
